faaf74a06969e7eca6c888838132bdc7dcc3cdcd
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2024-11-25 14:50:37 +0530

    Get and Setters class Note

--- textBookNote/src/notes/AppNotes.java
+++ textBookNote/src/notes/AppNotes.java
@@ -59,6 +59,27 @@ public class AppNotes implements NotesInterface {
             this.text = text;
             this.date = date;
         }
+
+        // Geters and Setters
+        public int getId() {return id;}
+        public void setId(int id) {this.id = id; }
+        public String getTitle() { return title;}
+        public void setTitle(String title) {this.title = title; }
+        public String getText() { return text;}
+        public void setText(String text) {this.text = text;}
+        public String getDate() { return date;}
+        public void setDate(String date) { this.date = date; }
+
+        @Override
+        public String toString(){
+            return "Note{" + 
+                    "id=" + id +
+                    ", title='" + title + '\'' + 
+                    ", text='" + text + '\'' +
+                    ", date='" + date + '\'' +
+                    '}';
+        }
+
     }
     
 }
